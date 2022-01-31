5d14b69f4b16bb1f2c6ab5c41fe77c2055955330
goldorak <goldorak@b001-04.etudiants.campus.villejuif>
2022-01-31 14:40:32 +0100

    commit of project files 1

---
+++ Project_DevMob/Project_DevMob/Project_DevMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_DevMobApp.swift
+//  Project_DevMob
+//
+//  Created by goldorak on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_DevMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
